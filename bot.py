784237f2e1c2d36c9627653fb78f1ced46e3a917
valien <valien.r@gmail.com>
2021-01-24 17:28:46 +0300

    hotfix teams command

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 __author__ = "Valien"
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 __maintainer__ = "Valien"
 __link__ = "https://github.com/rvalien"
 
@@ -50,6 +50,7 @@ class OrbbCommands(commands.Cog):
             await asyncio.sleep(20)
             msg = await ctx.channel.fetch_message(msg.id)
             # get reactors who react first emoji
+            logger.info(msg.reactions, msg.reactions[0].users().flatten(), msg.reactions[1].users().flatten())
             reactors = await msg.reactions[0].users().flatten()
             # remove bots
             reactors = list(filter(lambda x: not x.bot, reactors))
@@ -101,6 +102,7 @@ class OrbbCommands(commands.Cog):
         msg = await ctx.channel.fetch_message(msg.id)
         # get reactors who react first emoji
         reactors = await msg.reactions[0].users().flatten()
+        logger.info(msg.reactions, msg.reactions[0].users().flatten(), msg.reactions[1].users().flatten())
         # remove bots
         reactors = list(filter(lambda x: not x.bot, reactors))
         # get only names
@@ -168,7 +170,7 @@ class OrbbCommands(commands.Cog):
             await ctx.send("voice channel is empty", tts=False)
 
 
-logger.info("Программа стартует")
+logger.info("run")
 bot = commands.Bot(command_prefix="$")
 bot.add_cog(OrbbCommands(bot))
 bot.run(token)

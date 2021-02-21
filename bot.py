3c0aa53892118989669366b5b4d99e291d793229
valien <valien.r@gmail.com>
2021-02-21 19:23:42 +0300

    add bot status as play game

--- bot.py
+++ bot.py
@@ -5,28 +5,39 @@ __link__ = "https://github.com/rvalien/orbbot"
 
 from discord.ext import commands
 
+import discord
 import os
 import logging
 
-INITIAL_EXTENSIONS = [
-    "cogs.qc",
-    "cogs.listeners",
-    "cogs.commands"
-]
+INITIAL_EXTENSIONS = ["cogs.qc", "cogs.listeners", "cogs.commands"]
 token = os.environ["TOKEN"]
 prefix = os.environ["PREFIX"]
+# token = os.environ["TEST_TOKEN"]
+# prefix = "!"
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 logger.info("run")
-bot = commands.Bot(command_prefix=prefix)
+bot = commands.Bot(command_prefix=prefix, description="Small bot for lil QC community")
 
-for extension in INITIAL_EXTENSIONS:
-    try:
-        bot.load_extension(extension)
-        logger.info(f"load: {extension}\n")
-    except Exception as e:
-        logger.warning(f"Failed to load extension {extension}\n{type(e).__name__}: {e}")
 
-bot.run(token)
+@bot.event
+async def on_ready():
+    """http://discordpy.readthedocs.io/en/latest/api.html#discord.on_ready"""
+
+    print(f"\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n")
+    game = discord.Game("Жмурки")
+    await bot.change_presence(status=discord.Status.online, activity=game)
+    print("beep-boop i'm online...!")
+
+
+if __name__ == "__main__":
+    for extension in INITIAL_EXTENSIONS:
+        try:
+            bot.load_extension(extension)
+            logger.info(f"load: {extension}\n")
+        except Exception as e:
+            logger.warning(f"Failed to load extension {extension}\n{type(e).__name__}: {e}")
+
+    bot.run(token, bot=True, reconnect=True)

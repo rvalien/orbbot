72094c3ba1615544bca4504ec9380e13c91d089d
valien <valien.r@gmail.com>
2021-10-13 11:27:22 +0300

    remove talk command

--- cogs/dev.py
+++ cogs/dev.py
@@ -9,19 +9,6 @@ class DevCommands(commands.Cog):
         self.bot = bot
         self._last_member = None
 
-    @commands.command(aliases=["привет", "эй", "вопрос"])
-    async def talk(self, ctx, *, question):
-
-        url = "https://api.aicloud.sbercloud.ru/public/v1/public_inference/gpt3/predict"
-        payload = {"text": question}
-        headers = {"Content-Type": "application/json"}
-        response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
-        if response.status_code == 200 and "predictions" in response.json().keys():
-            await ctx.send(f'{response.json()["predictions"][:500]}...')
-        else:
-            print(f"{response.text=}{response.status_code=}")
-            await ctx.send("noope")
-
     @commands.command()
     @commands.is_owner()
     async def members(self, ctx):
